44423b5d0579b88734817adb923b9c6e468e329d
Prajil K <prajilk20017@gmail.com>
2025-04-23 15:16:07 +0530

    updated store

--- src/app/api/store/[storeId]/route.ts
+++ src/app/api/store/[storeId]/route.ts
@@ -1,9 +1,11 @@
+import { getPlaceDetails } from "@/lib/google";
 import { error400, error403, error500, success200 } from "@/lib/response";
 import { AuthenticatedRequest } from "@/lib/types/auth-request";
 import { isRestricted } from "@/lib/utils";
 import { withDbConnectAndAuth } from "@/lib/withDbConnectAndAuth";
 import { ZodStoreSchema } from "@/lib/zod-schema/schema";
 import Store from "@/models/storeModel";
+import { StoreDocument } from "@/models/types/store";
 
 async function updateHandler(
     req: AuthenticatedRequest,
@@ -13,42 +15,40 @@ async function updateHandler(
         if (isRestricted(req.user)) return error403();
 
         const data = await req.json();
-
-        if (!data) {
-            return error400("Invalid data format.", {});
-        }
+        if (!data) return error400("Invalid data format.");
 
         const { storeId } = await params;
 
         const result = ZodStoreSchema.safeParse(data);
+        if (!result.success) return error400("Invalid data format.");
 
-        if (result.success) {
-            const existingStore = await Store.findOne({
-                _id: storeId,
-            });
+        const existingStore = await Store.findById(storeId);
+        if (!existingStore) return error400("Store not found.");
 
-            if (!existingStore) {
-                return error400("User not found.");
-            }
+        let updateData: Partial<StoreDocument> = result.data;
 
-            const updatedStore = await Store.findOneAndUpdate(
-                { _id: storeId },
-                result.data,
-                { new: true }
-            );
-
-            return success200({ store: updatedStore });
+        if (existingStore.placeId !== result.data.placeId) {
+            const location = await getPlaceDetails(result.data.placeId);
+            if (!location) return error400("Unable to get coordinates.");
+            updateData = {
+                ...updateData,
+                lat: location.lat,
+                lng: location.lng,
+            };
         }
 
-        if (result.error) {
-            return error400("Invalid data format.", {});
-        }
+        const updatedStore = await Store.findByIdAndUpdate(
+            storeId,
+            updateData,
+            { new: true }
+        );
+        return success200({ store: updatedStore });
     } catch (error) {
-        if (error instanceof Error) {
-            return error500({ error: error.message });
-        } else {
-            return error500({ error: "An unknown error occurred" });
-        }
+        const message =
+            error instanceof Error
+                ? error.message
+                : "An unknown error occurred";
+        return error500({ error: message });
     }
 }
 
